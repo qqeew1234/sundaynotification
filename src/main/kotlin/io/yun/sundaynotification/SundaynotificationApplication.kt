22d3a596bf2f5cb3296b4e65460f0e4fdfb131ff
yun <qqeew1234@gmail.com>
2026-05-22 16:21:10 +0900

    feat(init): Project setup for Sunday Notification Service
    - 프로젝트 초기 설정 추가
    - Gradle 설정 파일 및 의존성 추가
    - Spring Boot 애플리케이션 기본 구조 생성
    - 테스트 코드 추가

---
+++ src/main/kotlin/io/yun/sundaynotification/SundaynotificationApplication.kt
@@ -0,0 +1,11 @@
+package io.yun.sundaynotification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SundaynotificationApplication
+
+fun main(args: Array<String>) {
+	runApplication<SundaynotificationApplication>(*args)
+}
---
+++ src/test/kotlin/io/yun/sundaynotification/SundaynotificationApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.yun.sundaynotification
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SundaynotificationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
